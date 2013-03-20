f1fc6273d0b1628e700173c8b5b154bcf405ca0d
Albert Lam <tell.albert@gmail.com>
2013-03-19 22:12:19 -0500

    finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,17 +4,29 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
