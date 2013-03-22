f839b6a7a19fc2886bbc64e2311d2a9d18549320
Albert Lam <tell.albert@gmail.com>
2013-03-22 09:29:03 -0500

    update routes table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
-  match '/help', to: 'static_pages#contact'
+  match '/contact', to: 'static_pages#contact'
   match '/about', to: 'static_pages#about'
   match '/service', to: 'static_pages#service'
 
